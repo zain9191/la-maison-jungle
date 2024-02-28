a127d2ba62128c54df6b48ab06241033b7f9b12a
Zain <zain.frayha@gmail.com>
2024-02-28 14:02:23 +0100

    Incorporez du style et des assets à votre projet

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,13 @@
+import logo from './assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
-    return <h1>La maison jungle</h1>
+    const titile = "La maison jungle"
+    return <div className='lmj-banner'>
+        <img src= {logo} alt = "La maison jungle" className='lmj-logo'></img>
+        <h1 className='lmj-titile'>{titile}</h1>   
+            
+            </div>
 }
 
 export default Banner
